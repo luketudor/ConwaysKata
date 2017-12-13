3c64ce2276c96cf55b692f385cb5f3828572b27d
luketudor <Luke.Tudor@myob.com>
2017-12-14 11:03:43 +1300

    Fix conflict

--- conways.js
+++ conways.js
@@ -3,21 +3,9 @@ function nextGridState(currentGridState) {
 }
 
 function countNeighbours(currentGridState) {
-<<<<<<< HEAD
-    let outputGrid = Object.assign([], currentGridState);
-<<<<<<< HEAD
-    outputGrid.forEach(cell => {
-        cell.numNeighbours = 3;
-    });
-=======
-    outputGrid.forEach(cell => cell.numNeighbours = howManyNeighbours(cell, currentGridState));
->>>>>>> Small refactor
-    return outputGrid;
-=======
     return Array.from(currentGridState, cell => {
         return {row: cell.row, col: cell.col, numNeighbours: howManyNeighbours(cell, currentGridState)};
     });
->>>>>>> Refactor neighbour counter to not modify original objects
 }
 
 function howManyNeighbours(gridElement, currentGridState) {
