b8fedd4987a2e9296468256198bb221eba06c60a
luketudor <Luke.Tudor@myob.com>
2017-12-18 10:30:02 +1300

    Reduce duplication by making neighbour verification function more generic

--- neighbourCounter.js
+++ neighbourCounter.js
@@ -8,16 +8,15 @@ function countNeighbours(cellsToCount, potentialNeighbours, keySeparator) {
 
 function howManyNeighbours(gridElement, currentGridState) {
     return currentGridState.filter(
-        cell => inNeighbouringRow(gridElement, cell) && inNeighbouringCol(gridElement, cell) && gridElement !== cell
+        cell =>
+            inNeighbouringField(gridElement, cell, 'row') &&
+            inNeighbouringField(gridElement, cell, 'col') &&
+            gridElement !== cell
     ).length;
 }
 
-function inNeighbouringRow(cell, otherCell) {
-    return Math.abs(cell.row - otherCell.row) < 2;
-}
-
-function inNeighbouringCol(cell, otherCell) {
-    return Math.abs(cell.col - otherCell.col) < 2;
+function inNeighbouringField(cell, otherCell, fieldName) {
+    return Math.abs(cell[fieldName] - otherCell[fieldName]) < 2;
 }
 
 module.exports.countNeighbours = countNeighbours;
