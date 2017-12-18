f56eb9bd376a2179e0bbee69cceaf85fafb865c4
luketudor <Luke.Tudor@myob.com>
2017-12-18 13:50:06 +1300

    Change app.js input formatting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let nextGridState = require('./conways').nextGridState;
 let printGrid = require('./conways').printGrid;
+let parseBitmap = require('./conways').parseBitmap;
 
 function sleep(milliseconds) {
     var start = new Date().getTime();
@@ -11,11 +12,17 @@ function sleep(milliseconds) {
 }
 
 console.log('Conway\'s Game of Life');
-let liveCells = new Set([
+/*let liveCells = new Set([
     {row: 0, col: 1},
     {row: 0, col: 2},
     {row: 0, col: 3},
     {row: -1, col: 2}
+]);*/
+let liveCells = parseBitmap([
+    [0, 1, 0],
+    [1, 1, 1],
+    [1, 0, 1],
+    [0, 1, 0]
 ]);
 for (let i = 0; i < 60; i++) {
     printGrid(liveCells);
